e6de4bfc7c125368256d844aebb2ac49873901cd
Oiro <moses.oiro.okello@gmail.com>
2016-09-20 18:32:03 +0300

    Sum of numbers, subtraction, multiplication and factorials

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,15 +10,15 @@ def subtract(a, b)
 
 end
 
-
-def sum(a = [])
+def sum(array)
+# def sum(a = [])
 # def sum(a = [x])
 
 
 
-	sum = 0
+	# sum = 0
 
-	a.inject(0) { |sum, x| sum + x.to_i }
+	# a.inject(0) { |sum, x| sum + x.to_i }
 
 	# sum = a
 
@@ -28,6 +28,7 @@ def sum(a = [])
 	# 0.upto(5) do |n|
 
 		# puts n + n
+		# puts n
 	  # puts n + 1
 
 	# end
@@ -37,12 +38,27 @@ def sum(a = [])
 	# 	puts n
 	# end
 
-	
-	
-		
-	
+	## mistrial
 
+	# total = 0
+	# print "The value of number is #{number}"
+	# numbers = []
+	# print "The value of numbers is #{numbers}"
+	# return 0 if numbers.empty?
+	# numbers.each { |number| total = number + total }
+
+
+	total = 0
+	if array.empty?
+		0
+	else
+		array.each { |element| puts total = element + total }
+		total
+	end
+
+# sum([])
 end
+# sum()
 
 def multiply(x, y)
 
@@ -72,5 +88,46 @@ def power(b, n)
 	b ** n
 
 end
+
+# def factorial(n)
+
+# 	if n == 0
+#     1
+#   else
+#     n * factorial(n-1)
+#   end
+
+# end
+
+# def factorial(number)
+# 	return 1 if number == 0
+	
+# 	number * factorial(number - 1)
+# end
+
+
+# def factorial(number)
+# total = 1
+# if number == 0
+# 1
+# else
+# number.downto(2) do |n|
+# total = total * n
+# end
+# total
+# end
+
+# end
+
+def factorial(number)
+	total = 1
+	return 1 if number == 0
+	number.downto(2) do |n|
+		total = total * n
+	end
+	total
+end
+
+
 
 
